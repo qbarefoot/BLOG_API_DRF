71317d28fb9ccfcccb9bc1cb1403219072757ac9
qbarefoot <quintenbarefoot@gmail.com>
2021-12-10 02:34:20 -0600

    permissions.py folder, and updated views for blogpost/list so other accounts can't delete posts

--- blog_python_api/urls.py
+++ blog_python_api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from blog_python_api import views
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('blogposts/', views.BlogPostList.as_view()),
--- blog_python_api/views.py
+++ blog_python_api/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics
 from blog_python_api import serializers
 from django.contrib.auth.models import User
 from blog_python_api.models import BlogPost
+from rest_framework import permissions
+from blog_python_api.permissions import IsOwnerOrReadOnly
+
 
 # Create your views here.
 
@@ -17,9 +20,15 @@ class UserDetail(generics.RetrieveAPIView):
 class BlogPostList(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = serializers.BlogPostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 class BlogPostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = serializers.BlogPostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                            IsOwnerOrReadOnly]
